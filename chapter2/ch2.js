40b77d2a17c74a41f0665d19c8173a91512bdeda
ntno <organick.natan@gmail.com>
2017-02-24 20:35:22 -0500

    moved basic script to its own javascript file

---
+++ chapter2/ch2.js
@@ -0,0 +1,15 @@
+'use strict';
+var that = this
+	,msg = "hi, friend!"
+	,count = 0
+	,printMessage = function(){
+		console.log(that.msg);
+	}
+	,printCount = function(){
+		console.log(that.count);
+	}
+	,incrementCount = function(){
+		that.count = that.count + 1;
+	};
+
+console.log("woah console printing");
