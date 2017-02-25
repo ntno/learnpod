d2f1e6df29b32d92bfc2c5006e605fecdc6478d8
ntno <organick.natan@gmail.com>
2017-02-25 17:19:57 -0500

    jquery - update span as variable is changed

--- chapter3/js/ch3.js
+++ chapter3/js/ch3.js
@@ -10,8 +10,13 @@ var that = this
 	}
 	,incrementCount = function(){
 		that.count = that.count + 1;
+		that.countDisplaySpan.html(count);	
 	}
 	,countDisplaySpan;
 
 console.log("woah console printing");
+
 $('#messageDisplay').text(msg);
+
+that.countDisplaySpan = $('#countDisplay');
+that.countDisplaySpan.html(count);				
