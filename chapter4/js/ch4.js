de7c0719ceb5cdc22bfb786383b2b2e43148c79c
ntno <organick.natan@gmail.com>
2017-05-05 20:49:46 -0400

    bound function and variables

--- chapter4/js/ch4.js
+++ chapter4/js/ch4.js
@@ -4,4 +4,11 @@ var learnpodApp = angular.module('learnpodApp', []);
 
 learnpodApp.controller('learnpodController', function($scope){
 	$scope.title = "learnpod";
+	$scope.count = 0;
+	$scope.incrementCount = function(){
+		$scope.count += 1; 
+	};
+	$scope.printCount = function(){
+		console.log($scope.count);
+	}
 });
