a97d2c72536b7dcd539181048b9ab82e92f1cf6c
ntno <organick.natan@gmail.com>
2017-05-05 20:33:15 -0400

    angular setup

---
+++ chapter4/js/ch4.js
@@ -0,0 +1,9 @@
+'use strict'
+
+console.log('ch4.js loaded');
+
+var learnpodApp = angular.module('learnpodApp', []);
+
+learnpodApp.controller('learnpodController', function(){
+	console.log('learnpodController registered');
+});
