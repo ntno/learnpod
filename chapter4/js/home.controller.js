824cccb72b42f1e79c838599f1499b53b93259d6
ntno <organick.natan@gmail.com>
2017-05-05 21:43:52 -0400

    added contact controller to home.controller.js

--- chapter4/js/home.controller.js
+++ chapter4/js/home.controller.js
@@ -1,6 +1,6 @@
 (function(){
 'use strict'
-//the controllers module contains the HomeController component
+//the controllers module contains the HomeController component and ContactController
 angular.module('learnpod.controllers')
 	.controller('HomeController', function($scope){
 		$scope.title = "learnpod home";
@@ -11,5 +11,12 @@ angular.module('learnpod.controllers')
 		$scope.printCount = function(){
 			console.log($scope.count);
 		}
+	})
+	
+	.controller('ContactController', function($scope){
+		$scope.title = "contact us";
+		$scope.phone = "(123) 456-7890";
+		$scope.email = "ntno@github"
 	});
+
 })();
