f4eef3760e10386becb48ccd60b02a9024f52ad1
ntno <organick.natan@gmail.com>
2017-05-05 21:37:55 -0400

    separated app and controller modules

---
+++ chapter4/js/app.js
@@ -0,0 +1,7 @@
+(function(){
+'use strict'
+//the app depends on the controllers module
+angular.module('learnpodApp', ['learnpod.controllers']);
+angular.module('learnpod.controllers', []);
+
+})();
---
+++ chapter4/js/home.controller.js
@@ -0,0 +1,15 @@
+(function(){
+'use strict'
+//the controllers module contains the HomeController component
+angular.module('learnpod.controllers')
+	.controller('HomeController', function($scope){
+		$scope.title = "learnpod home";
+		$scope.count = 0;
+		$scope.incrementCount = function(){
+			$scope.count += 1; 
+		};
+		$scope.printCount = function(){
+			console.log($scope.count);
+		}
+	});
+})();
