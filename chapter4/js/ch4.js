9a97a9928f28c54608e260d20d1c51cdbc2c701c
ntno <organick.natan@gmail.com>
2017-05-05 20:35:37 -0400

    instantiated controller

--- chapter4/js/ch4.js
+++ chapter4/js/ch4.js
@@ -5,5 +5,5 @@ console.log('ch4.js loaded');
 var learnpodApp = angular.module('learnpodApp', []);
 
 learnpodApp.controller('learnpodController', function(){
-	console.log('learnpodController registered');
+	console.log('learnpodController instantiated');
 });
