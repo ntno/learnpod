101fd80ab3d1e9ade0a8db8a79b3784e89ac0953
ntno <organick.natan@gmail.com>
2017-05-13 17:37:16 -0400

    more tests for contact controller

--- chapter6/test/js/contact.controller.spec.js
+++ chapter6/test/js/contact.controller.spec.js
@@ -14,9 +14,29 @@
         });
       }));
 
-      it('it should have email set to ntno@github on scope', function () {
+      it('it should have phone set to "(123) 456-7890" on scope', function () {
         expect(mockContactControllerScope.contactScope.data.phone).toEqual('(123) 456-7890');
       });
+
+      it('it should have email set to "ntno@github" on scope', function () {
+        expect(mockContactControllerScope.contactScope.data.email).toEqual('ntno@github');
+      });
+
+      it('it should have title set to "contact us" on scope', function () {
+        expect(mockContactControllerScope.contactScope.data.title).toEqual('contact us');
+      });
+
+      it('it should have user\'s email set to "" on scope', function () {
+        expect(mockContactControllerScope.contactScope.userData.email).toEqual('');
+      });
+
+      describe('When submit is called', function(){
+        it('should exist', function(){
+          expect(mockContactControllerScope.contactScope.userFunctions.submit)
+            .toEqual(jasmine.any(Function));
+        });
+      });
+
     });
   });
 })();
