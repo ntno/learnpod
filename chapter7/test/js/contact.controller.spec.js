765da2f3b7bc53162ee9ed096e87be4300817f32
ntno <organick.natan@gmail.com>
2017-05-14 12:38:45 -0400

    refactoring and more tests for contact controller

--- chapter7/test/js/contact.controller.spec.js
+++ chapter7/test/js/contact.controller.spec.js
@@ -3,7 +3,8 @@
 
     describe('When ContactController is instantiated', function () {
       var mockContactControllerScope
-      ,mockContactController;
+      ,mockContactController
+      ,mockScope;
 
       beforeEach(module('learnpodApp'));
 
@@ -12,28 +13,45 @@
         mockContactController = $controller('ContactController', {
             '$scope': mockContactControllerScope
         });
+        mockScope = mockContactControllerScope.contactScope;
       }));
 
       it('it should have phone set to "(123) 456-7890" on scope', function () {
-        expect(mockContactControllerScope.contactScope.data.phone).toEqual('(123) 456-7890');
+        expect(mockScope.data.phone).toEqual('(123) 456-7890');
       });
 
       it('it should have email set to "ntno@github" on scope', function () {
-        expect(mockContactControllerScope.contactScope.data.email).toEqual('ntno@github');
+        expect(mockScope.data.email).toEqual('ntno@github');
       });
 
       it('it should have title set to "contact us" on scope', function () {
-        expect(mockContactControllerScope.contactScope.data.title).toEqual('contact us');
+        expect(mockScope.data.title).toEqual('contact us');
       });
 
       it('it should have user\'s email set to "" on scope', function () {
-        expect(mockContactControllerScope.contactScope.userData.email).toEqual('');
+        expect(mockScope.userData.email).toEqual('');
       });
 
       describe('When submit is called', function(){
-        it('should exist', function(){
-          expect(mockContactControllerScope.contactScope.userFunctions.submit)
+        it('it should exist', function(){
+          expect(mockScope.userFunctions.submit)
             .toEqual(jasmine.any(Function));
+        });
+
+        it('it should set data.email to userData.email', function(){
+          var userEmail = "user@gmail"
+          mockScope.userData.email = userEmail;
+          mockScope.userFunctions.submit();
+          expect(mockScope.data.email)
+            .toEqual(userEmail);
+        });
+
+        it('it should reset userData.email to ""', function(){
+          var userEmail = "user@gmail"
+          mockScope.userData.email = userEmail;
+          mockScope.userFunctions.submit();
+          expect(mockScope.userData.email)
+            .toEqual("");
         });
       });
 
