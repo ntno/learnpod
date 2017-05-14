3c51a1b03cf53e5396e38b1b6af25b38f006b26f
ntno <organick.natan@gmail.com>
2017-05-14 13:27:37 -0400

    comment fix

--- karma.conf.js
+++ karma.conf.js
@@ -14,7 +14,6 @@ module.exports = function(config) {
 
 
     // list of files / patterns to load in the browser
-    //should use local file but for tutorial I will point to CDN
     files: [
       'node_modules/angular/angular.min.js',
       'node_modules/angular-route/angular-route.min.js',
