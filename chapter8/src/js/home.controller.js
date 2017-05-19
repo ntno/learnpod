c8ed7d8e5db54aca82854781aecf677e03fefb1f
ntno <organick.natan@gmail.com>
2017-05-19 16:27:40 -0400

    gif flipper

--- chapter8/src/js/home.controller.js
+++ chapter8/src/js/home.controller.js
@@ -3,7 +3,29 @@
 //the controllers module contains the HomeController component and ContactController
 angular.module('home.controller', ['dataResource.factory', 'gifInfo.factory'])
 	.controller('HomeController', ['$scope', 'DataResourceFactory', 'GifInfoFactory', function($scope, DataResourceFactory, GifInfoFactory){
-		var homeScope = null;
+		var homeScope = null
+		,giphyData = null
+		,getGifs = function(){
+			DataResourceFactory.getGiphySearchResource().get(
+				function(successfulResponse){
+					giphyData = successfulResponse.data;
+					console.log(giphyData);
+					homeScope.data.gif.url = giphyData[0].url;
+					homeScope.data.gif.imgUrl = giphyData[0].images.fixed_height.url;
+					homeScope.hasError = false;
+				},
+				function(failedResponse){
+					homeScope.hasError = true;
+				}
+			);
+		}
+		,currentGifIdx = 0
+		,nextGif = function(){
+			currentGifIdx++;
+			var idx = currentGifIdx%25;
+			homeScope.data.gif.url = giphyData[idx].url;
+			homeScope.data.gif.imgUrl = giphyData[idx].images.fixed_height.url;
+		};
 
 
 		//Establish Scope
@@ -16,23 +38,13 @@ angular.module('home.controller', ['dataResource.factory', 'gifInfo.factory'])
 				}
 			}
 			,userFunctions : {
+				nextGif : nextGif
 			}
 			,hasError : null
 		};
 
 		//Initialize
 		homeScope = $scope.homeScope;
-
-		DataResourceFactory.getGiphySearchResource().get(
-			function(successfulResponse){
-				var giphyData = successfulResponse.data;
-				homeScope.data.gif.url = giphyData[0].url; 
-				homeScope.data.gif.imgUrl = giphyData[0].images.fixed_height.url;
-				homeScope.hasError = false;
-			},
-			function(failedResponse){
-				homeScope.hasError = true;
-			}
-		);
+		getGifs();
 	}]);
 })();
--- chapter8/test/js/home.controller.spec.js
+++ chapter8/test/js/home.controller.spec.js
@@ -27,18 +27,33 @@
 
       describe('When HomeController is instantiated', function(){
           var mockGiphyResource = 'http://api.giphy.com/v1/gifs/search?q=cute+puppy&api_key=dc6zaTOxFJmzC'
+              ,firstGif = {
+                            type : "gif"
+                            ,url : "http://giphy.com/gifs/puppy-down-slides-gZLl9szOpgbpS"
+                            ,images : {
+                              fixed_height : {
+                                url : "http://media4.giphy.com/media/gZLl9szOpgbpS/200.gif"
+                                ,width : 155
+                                ,height : 200
+                              }
+                            }
+                          }
+              ,secondGif = {
+                            type : "gif"
+                            ,url : "http://giphy.com/gifs/stop-slow-mo-14ivBLRRRmyQw0"
+                            ,images : {
+                              fixed_height : {
+                                url : "http://media2.giphy.com/media/14ivBLRRRmyQw0/200.gif"
+                                ,width : 200
+                                ,height : 200
+                              }
+                            }
+                          }
               ,mockGiphyResponse = {
-                data : [{
-                  type : "gif"
-                  ,url : "http://giphy.com/gifs/puppy-down-slides-gZLl9szOpgbpS"
-                  ,images : {
-                    fixed_height : {
-                      url : "http://media4.giphy.com/media/gZLl9szOpgbpS/200.gif"
-                      ,width : 155
-                      ,height : 200
-                    }
-                  }
-                }]
+                data : [
+                  firstGif
+                  ,secondGif
+                ]
               };
 
           beforeEach(inject(function ($injector) {
