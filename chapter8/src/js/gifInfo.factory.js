29457d8daea1cbc067427871e23a49fcfdc50626
ntno <organick.natan@gmail.com>
2017-05-20 18:21:22 -0400

    added gifInfo functionality

--- chapter8/src/js/gifInfo.factory.js
+++ chapter8/src/js/gifInfo.factory.js
@@ -3,7 +3,19 @@
   angular.module('gifInfo.factory', [])
     .factory('GifInfoFactory', [function() {
       var createGifInfo = function(data) {
-        return data;
+        var learnpodGifData = {}
+        ,giphyFixedHeight;
+
+        if(data !== undefined && data !== null && data.images !== undefined){
+          giphyFixedHeight = data.images.fixed_height;
+          learnpodGifData.sourceUrl = data.url;
+          learnpodGifData.fixed_height = {};
+          learnpodGifData.fixed_height.url = giphyFixedHeight.url;
+          learnpodGifData.fixed_height.height = giphyFixedHeight.height;
+          learnpodGifData.fixed_height.width = giphyFixedHeight.width;
+        }
+
+        return learnpodGifData;
       };
       return {
         createGifInfo : createGifInfo
